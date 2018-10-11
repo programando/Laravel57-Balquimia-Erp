8058a8fbb5d367b5899539a8ecbed8418b929f1d
programando <jhonjamesmg@hotmail.com>
2018-10-11 10:08:12 -0500

    Login II

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use View;
+use view;
 class AppServiceProvider extends ServiceProvider
 {
     /**
--- resources/views/adminModern1/navbar-top/icon-maximizar.blade.php
+++ resources/views/adminModern1/navbar-top/icon-maximizar.blade.php
@@ -1,1 +1,5 @@
-<li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="#"><i class="ficon ft-maximize"></i></a></li>
+<li class="nav-item d-none d-md-block">
+  <a class="nav-link nav-link-expand" href="#">
+    <i class="ficon ft-maximize"></i>
+  </a>
+</li>
